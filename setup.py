c690b60ef930758b222f09324ed949b5d13792c6
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-09-16 14:04:15 +0100

    Get version from git annotated tag (#26)
    * get version and build number from github
    
    * Create version.py
    
    * Get version from github
    
    * Get version from github
    
    * Add missing import
    
    * fix getting GIT_DESCRIBE_NUMBER
    
    * add fetch-depth: 0
    
    * normalises the version from git describe to pep440
    
    * Update build settings
    
    * Update setup.py
    
    * Update setup.py
    
    * uses pep440 version in conda and pip cases
    
    * Revert "uses pep440 version in conda and pip cases"
    
    This reverts commit cb35fb1c3d3cfe0891c701fd9be309001fd4844c.
    
    * Add source

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup
 import os
+import subprocess
+
 try:
     from sphinx.setup_command import BuildDoc
     sphinx_available = False
@@ -10,25 +12,47 @@ try:
 except ImportError as ie:
     sphinx_available = False
     cmdclass = {}
+    
+def version2pep440(version):
+    '''normalises the version from git describe to pep440
+    
+    https://www.python.org/dev/peps/pep-0440/#id29
+    '''
+    if version[0] == 'v':
+        version = version[1:]
 
-import re
+    if u'-' in version:
+        v = version.split('-')
+        v_pep440 = "{}.dev{}".format(v[0], v[1])
+    else:
+        v_pep440 = version
+
+    return v_pep440
+
+
+git_version_string = subprocess.check_output('git describe', shell=True).decode("utf-8")[1:]
+    
 
 #with open("README.rst", "r") as fh:
 #    long_description = fh.read()
-long_description = 'A number of templates and tools to develop Qt GUI\'s with Python effectively.'
+long_description = 'A number of templates and tools to develop Qt GUIs with Python effectively.'
 
-with open('eqt/__init__.py') as fd:
-    version = re.search("__version__ = '(.*)'", fd.read()).group(1)
-
-if 'CONDA_BUILD' in os.environ.keys():
-    install_requires = []
-else:
+if os.environ.get('CONDA_BUILD', 0) == 0:
     install_requires = [
     
         'sphinx',
         'pyside2'
 
     ]
+    version = version2pep440(git_version_string)
+    
+else:
+    # if it is a conda build requirements are going to be satisfied by conda
+    install_requires = []
+    version = git_version_string
+
+    
+print ('version {}'.format(version))
 
 name = "eqt"
 
