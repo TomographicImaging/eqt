9fcb38b41b7cd4078223fe89823bcf980d07fe21
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-09-16 11:01:42 +0100

    normalises the version from git describe to pep440

--- setup.py
+++ setup.py
@@ -12,18 +12,35 @@ try:
 except ImportError as ie:
     sphinx_available = False
     cmdclass = {}
+    
+def version2pep440(version):
+    '''normalises the version from git describe to pep440
+    
+    https://www.python.org/dev/peps/pep-0440/#id29
+    '''
+    if version[0] == 'v':
+        version = version[1:]
+
+    if u'-' in version:
+        v = version.split('-')
+        v_pep440 = "{}.dev{}".format(v[0], v[1])
+    else:
+        v_pep440 = version
+
+    return v_pep440
+
+
+git_version_string = subprocess.check_output('git describe', shell=True).decode("utf-8").rstrip()
+    
 
 #with open("README.rst", "r") as fh:
 #    long_description = fh.read()
 long_description = 'A number of templates and tools to develop Qt GUIs with Python effectively.'
-    
-cmd = 'git describe'
-dversion = subprocess.check_output(cmd, shell=True).strip().decode('utf-8')[1:]
-
-print ('version {}'.format(dversion))
 
 if 'CONDA_BUILD' in os.environ.keys():
+    # if it is a conda build requirements are going to be satisfied by conda
     install_requires = []
+    version = git_version_string
 else:
     install_requires = [
     
@@ -31,11 +48,14 @@ else:
         'pyside2'
 
     ]
+    version = version2pep440(git_version_string)
+    
+print ('version {}'.format(version))
 
 name = "eqt"
 
 setup(name=name,
-      version = dversion,
+      version = version,
       description = 'A number of templates and tools to develop Qt GUIs with Python effectively',
       long_description = long_description,
       author = 'Edoardo Pasca',
@@ -53,6 +73,6 @@ setup(name=name,
       command_options={
         'build_sphinx': {
             'project': ('setup.py', name),
-            'version': ('setup.py', dversion),
+            'version': ('setup.py', version),
             'source_dir': ('setup.py', 'doc')}},
       )
