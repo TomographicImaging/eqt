8d8413cb0a0c746dc546f25eafd50686505609f9
Edoardo Pasca <edo.paskino@gmail.com>
2021-10-28 13:49:22 +0100

    fix import if conda build

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,4 +1,14 @@
 import unittest
+import os
+
+# skip the tests on GitHub actions
+if os.environ.get('CONDA_BUILD', 0) == '1':
+    skip_as_conda_build = True
+else:
+    skip_as_conda_build = False
+    from dialog_example_2_test import MainUI, DialogTest
+
+print ("skip_as_conda_build is set to ", skip_as_conda_build)
 
 class TestModuleBase(unittest.TestCase):
     def test_version(self):
