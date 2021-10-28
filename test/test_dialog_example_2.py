f69cfc093a893d4660af6cca9b9f5b4fc9b090a7
Edoardo Pasca <edo.paskino@gmail.com>
2021-10-28 13:15:16 +0100

    skip MaiUI if conda build

--- test/test_dialog_example_2.py
+++ test/test_dialog_example_2.py
@@ -1,80 +1,22 @@
-from PySide2 import QtCore, QtWidgets, QtTest
+from PySide2 import QtWidgets
 from PySide2.QtTest import QTest
 from PySide2.QtCore import Qt
 import glob
 import sys
 import os
-from eqt.ui import UIFormFactory, FormDialog
 import unittest
 
 # skip the tests on GitHub actions
 if os.environ.get('CONDA_BUILD', 0) == 1:
+    from common import MainUI
     skip_as_conda_build = True
+
 else:
     skip_as_conda_build = False
 
 print ("skip_as_conda_build is set to ", skip_as_conda_build)
 
-class MainUI(QtWidgets.QMainWindow):
 
-    def __init__(self, parent=None):
-        QtWidgets.QMainWindow.__init__(self, parent)
-
-        pb = QtWidgets.QPushButton(self)
-        pb.setText("Open Dialog with form layout")
-        pb.clicked.connect(lambda: self.openFormDialog())
-
-        layout = QtWidgets.QHBoxLayout()
-        layout.addWidget(pb)
-        widg = QtWidgets.QWidget()
-        widg.setLayout(layout)
-        self.push_button = pb
-
-        self.setCentralWidget(widg)
-
-        self.show()
-
-    def openFormDialog(self):
-
-        dialog = FormDialog(parent=self, title='Example')
-        dialog.Ok.clicked.connect(lambda: self.accepted())
-        dialog.Cancel.clicked.connect(lambda: self.rejected())
-
-        # Example on how to add elements to the
-        # add input 1 as QLineEdit
-        qlabel = QtWidgets.QLabel(dialog.groupBox)
-        qlabel.setText("Input 1: ")
-        qwidget = QtWidgets.QLineEdit(dialog.groupBox)
-        qwidget.setClearButtonEnabled(True)
-        # finally add to the form widget
-        dialog.addWidget(qwidget, qlabel, 'input1', layout='form')
-
-        # add input 2 as QComboBox
-        qlabel = QtWidgets.QLabel(dialog.groupBox)
-        qlabel.setText("Input 2: ")
-        qwidget = QtWidgets.QComboBox(dialog.groupBox)
-        qwidget.addItem("option 1")
-        qwidget.addItem("option 2")
-        qwidget.setCurrentIndex(0)
-        qwidget.setEnabled(True)
-        # finally add to the form widget
-        dialog.addWidget(qwidget, qlabel, 'input2')
-
-        # store a reference
-        self.dialog = dialog
-
-        dialog.exec()
-
-    def accepted(self):
-        print("accepted")
-        print(self.dialog.widgets['input1_field'].text())
-        print(self.dialog.widgets['input2_field'].currentText())
-
-        self.dialog.close()
-
-    def rejected(self):
-        print("rejected")
-        self.dialog.close()
 
 
 _instance = None
