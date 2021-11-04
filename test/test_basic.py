a9f8aace5ac0b107ee5a89c0745df6d66d3bfc7d
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-11-04 14:25:08 +0000

    Tidy up version import unit test (#42)
    * Test version on conda as well
    
    * Remove conda check
    
    * Reinstate other import tests
    
    * change check for conda_build
    
    * comparison typo
    
    * Add -v to unit test command

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,14 +1,9 @@
 import unittest
 import os
 
-# skip the tests on GitHub actions
-if os.environ.get('CONDA_BUILD', 0) == '1':
-    skip_as_conda_build = True
-else:
-    skip_as_conda_build = False
+# skip test of UI imports on GitHub actions
+if os.environ.get('CONDA_BUILD', '0') != '1':
     from dialog_example_2_test import MainUI, DialogTest
-
-print ("skip_as_conda_build is set to ", skip_as_conda_build)
 
 class TestModuleBase(unittest.TestCase):
     def test_version(self):
