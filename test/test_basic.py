4773a5a957be014f84b72104a11da92b661b1123
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-11-04 13:57:38 +0000

    change check for conda_build

--- test/test_basic.py
+++ test/test_basic.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 # skip test of UI imports on GitHub actions
-if os.environ.get('CONDA_BUILD', '0') == '0':
+if os.environ.get('CONDA_BUILD', '0') =! '1':
     from dialog_example_2_test import MainUI, DialogTest
 
 class TestModuleBase(unittest.TestCase):
