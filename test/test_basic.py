7dac6972f2c3f2059e50c8a17b7374610d67366d
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-11-04 11:33:41 +0000

    Reinstate other import tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,6 +1,10 @@
 import unittest
 import os
 
+# skip test of UI imports on GitHub actions
+if os.environ.get('CONDA_BUILD', '0') == '0':
+    from dialog_example_2_test import MainUI, DialogTest
+
 class TestModuleBase(unittest.TestCase):
     def test_version(self):
         try:
