4dc7e3703902ab5e673410dcc0b79bb53cbbcbdc
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2023-02-09 15:20:52 +0000

    tidy up io

--- eqt/io.py
+++ eqt/io.py
@@ -14,17 +14,7 @@ def zip_directory(directory, compress=True, **kwargs):
         compress : bool
             Whether to compress the directory.
         '''
-
-
-
         zip = zipfile.ZipFile(directory + '.zip', 'a')
-
-        # Could potentially be used for reporting progress:
-        # total_size = 0
-        # for dirpath, dirnames, filenames in os.walk(directory):
-        #     for f in filenames:
-        #         fp = os.path.join(dirpath, f)
-        #         total_size += os.path.getsize(fp)
 
         if compress:
             compress_type = zipfile.ZIP_DEFLATED
@@ -33,9 +23,7 @@ def zip_directory(directory, compress=True, **kwargs):
 
         for r, d, f in os.walk(directory):
             for _file in f:
-                fname = os.path.join(r, _file)
-                arcname = fname[len(directory)+1:]
-                zip.write(fname, arcname, compress_type=compress_type)
-                # Could potentially be used for reporting progress:
-                #progress_callback.emit(os.path.getsize(fname)/total_size)
+                filepath = os.path.join(r, _file)
+                arcname = os.path.relpath(filepath, directory)
+                zip.write(filepath, arcname, compress_type=compress_type)
         zip.close() 
