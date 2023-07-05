a57a443b10e0ebc3b2d80a5f8002c89f69b274d9
Casper da Costa-Luis <casper.dcl@physics.org>
2023-07-05 16:56:16 +0100

    migrate setup.py => pyproject.toml

--- eqt/__init__.py
+++ eqt/__init__.py
@@ -1,3 +1,10 @@
-from eqt import version as dversion
+# version detector. Precedence: installed dist, git, 'UNKNOWN'
+try:
+    from ._dist_ver import __version__
+except ImportError:
+    try:
+        from setuptools_scm import get_version
 
-__version__ = dversion.version
+        __version__ = get_version(root="..", relative_to=__file__)
+    except (ImportError, LookupError):
+        __version__ = "UNKNOWN"
--- test/test_basic.py
+++ test/test_basic.py
@@ -6,11 +6,4 @@ if os.environ.get('CONDA_BUILD', '0') != '1':
     from dialog_example_2_test import MainUI, DialogTest
 
 class TestModuleBase(unittest.TestCase):
-    def test_version(self):
-        try:
-            from eqt import version as dversion
-            a = dversion.version
-            print ("version", a)
-            self.assertTrue(isinstance(a, str))
-        except ImportError as ie:
-            self.assertFalse(True, str(ie))
+    pass
