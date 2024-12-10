0cfa2108534e8fd47185a002c3837bf7fbe7fda4
Danica Sugic <danica.sugic@stfc.ac.uk>
2024-12-10 17:26:32 +0000

    Edit broken example

--- examples/dialog_save_state_example.py
+++ examples/dialog_save_state_example.py
@@ -35,7 +35,7 @@ class MainUI(QtWidgets.QMainWindow):
         dialog.addWidget(QtWidgets.QSlider(), 'Slider: ', 'slider')
         dialog.addWidget(UISliderWidget(QtWidgets.QLabel()), 'UISliderWidget: ', 'uiSliderWidget')
         dialog.addWidget(QtWidgets.QRadioButton('test 1'), 'RadioButton 1: ', 'radioButton')
-        dialog.addWidget(QtWidgets.QRadioButton('test 2'), 'RadioButton 2: ', 'radioButton')
+        dialog.addWidget(QtWidgets.QRadioButton('test 2'), 'RadioButton 2: ', 'radioButton_2')
         dialog.addWidget(QtWidgets.QTextEdit('test'), 'TextEdit: ', 'textEdit')
         dialog.addWidget(QtWidgets.QPlainTextEdit('test'), 'PlainTextEdit: ', 'plainTextEdit')
         dialog.addWidget(QtWidgets.QLineEdit('test'), 'LineEdit: ', 'lineEdit')
